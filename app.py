773ab29595885ea016c06012ad8dabd084d4401e
pavarit1908 <pavarit.tan@pccp.ac.th>
2026-02-11 01:10:11 +0700

    final fix

--- app.py
+++ app.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import os
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
-import numpy as np
-import streamlit as st
-
-st.header('Wound classification')
-model = load_model('C:\diabetic wound\Wound_classify.keras')
-data_cat = ['Mild diabetes',
- 'Moderate diabetes',
- 'Not a diabetic wound',
- 'Severe diabetes']
-img_width = 180
-img_height = 180
-image = st.text_input ("Enter image name (Please avoid using backslash!)", r"C:\Users\User\Downloads\Vascular wound\testing_set\Moderate vascular wound\สำเนาของ leg-ulcers-0024.jpg")
-
-image_load = tf.keras.utils.load_img(image, target_size=(img_height,img_width))
-img_arr = tf.keras.utils.array_to_img(image_load)
-img_bat = tf.expand_dims(img_arr,0)
-
-predict = model.predict(img_bat)
-
-score = tf.nn.softmax(predict)
-st.image(image)
-st.write('Wound in image is {} with an accuracy of {:0.2f}%'.format(data_cat[np.argmax(score)],np.max(score)*100))
-
-predicted_label = data_cat[np.argmax(score)]
-if predicted_label == str('Mild diabetes'):
-    st.write('Go get your blood checked immediately')
-elif predicted_label == str('Moderate diabetes'):
-    st.write('Go to a hospital when available')
-elif predicted_label == str('Not a diabetic wound'):
-    st.write("Cover it up with alchohol and don't let water get to it")
-else:
-    st.write("Go to a hospital as soon as possible")
-=======
 import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
@@ -100,4 +60,4 @@ if image is not None:
         - Maintain strict wound hegiene.           
         - Moniter the wound daily under medical supervision AT ALL TIMES.
         """)
->>>>>>> 8111f2c636f3ef9f8e4ec1f8607c321061c162e4
+ 
